56a7c5605dd7f67b2d1bdc0fbc3e99015fdf3cca
diggythecandygun <suarez7592@hotmail.com>
2022-02-17 22:51:56 +0100

    Resolve assemblies

--- Program.cs
+++ Program.cs
@@ -11,11 +11,25 @@ using Siemens.Engineering.HW;
 using System.Windows;
 using System.Globalization;
 using System.Threading;
+using System.Reflection;
 
 namespace ArchivarTIA
 {
     class Program
     {
+        private static TiaPortal tia;
+        private static string plc;
+        private static string hmi;
+
+        
+
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(60, 10);
+            List<string> proyectos = BuscarProyectos();
+            ArchivarProyectos1(proyectos);
+        }
+
         /// <summary>
         /// Devuelve una lista de paths a los proyectos encontrados
         /// </summary>
@@ -33,7 +47,7 @@ namespace ArchivarTIA
                 List<string> archivos = new List<string>(Directory.EnumerateFiles(dir));
                 foreach (var archivo in archivos)
                 {
-                    if (Path.GetExtension(archivo) == ".ap16") 
+                    if (Path.GetExtension(archivo) == ".ap16")
                     {
                         proyectos.Add(Path.GetFullPath(archivo));
                         count++;
@@ -66,7 +80,7 @@ namespace ArchivarTIA
             try
             {
                 Console.WriteLine("Abriendo TIA Portal");
-                TiaPortal tia = new TiaPortal(TiaPortalMode.WithoutUserInterface);
+                tia = new TiaPortal(TiaPortalMode.WithoutUserInterface);
                 ProjectComposition tiaProjects = tia.Projects;
 
                 foreach (var proyecto in proyectos)
@@ -82,7 +96,7 @@ namespace ArchivarTIA
 
 
                     try
-                    {                       
+                    {
                         FileInfo directorio = new FileInfo(proyecto);
                         Console.WriteLine($"Archivando proyecto {Path.GetFileNameWithoutExtension(proyecto)}");
                         Project project = tiaProjects.Open(directorio);
@@ -110,14 +124,83 @@ namespace ArchivarTIA
             }
         }
 
-        static void Main(string[] args)
+        static void ArchivarProyectos1(List<string> proyectos)
         {
 
-            Console.SetWindowSize(60, 10);
-            List<string> proyectos = BuscarProyectos();
-            ArchivarProyectos(proyectos);
+            //Subscribe to Assembly resolve event. Event fires when any assembly binding fails.
+            AppDomain.CurrentDomain.AssemblyResolve += OnAssemblyResolve;
 
-            
+            //Path to registry is HKEY_LOCAL_MACHINE\SOFTWARE\Siemens\Automation\Openness\*version\PublicAPI\*assembly
+            List<string> versions = RegistryReader.GetVersions();
+
+            int nr = 1;
+            List<string> assemblies = RegistryReader.GetAssemblies(versions[nr - 1]);
+            RegistryReader.GetAssemblyPath(versions[nr - 1], assemblies.Last(), out plc, out hmi);
+            try
+            {
+                Console.WriteLine("Abriendo TIA Portal");
+                tia = new TiaPortal(TiaPortalMode.WithoutUserInterface);
+                ProjectComposition tiaProjects = tia.Projects;
+
+                foreach (var proyecto in proyectos)
+                {
+                    Console.Clear();
+                    string nombreProyecto = Path.GetFileNameWithoutExtension(proyecto);
+                    DateTime moment = DateTime.Now;
+                    string fecha = $"{moment.Year}{moment.Month}{moment.Day}_{moment.Hour}{moment.Minute}";
+                    nombreProyecto = nombreProyecto + "_" + fecha;
+                    if (Path.GetExtension(proyecto) == ".ap16") nombreProyecto = nombreProyecto + ".zap16";
+                    else if (Path.GetExtension(proyecto) == ".ap15") nombreProyecto = nombreProyecto + ".zap15";
+                    else if (Path.GetExtension(proyecto) == ".ap15_1") nombreProyecto = nombreProyecto + ".zap15_1";
+
+
+                    try
+                    {
+                        FileInfo directorio = new FileInfo(proyecto);
+                        Console.WriteLine($"Archivando proyecto {Path.GetFileNameWithoutExtension(proyecto)}");
+                        Project project = tiaProjects.Open(directorio);
+                        DirectoryInfo target = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+                        project.Archive(target, $"{nombreProyecto}", ProjectArchivationMode.Compressed);
+                        project.Close();
+                        Console.WriteLine($"{nombreProyecto} archivado correctamente");
+                        Thread.Sleep(1000);
+                    }
+
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Imposible archivar proyecto {nombreProyecto}");
+                        Console.WriteLine(e);
+                        Console.ReadKey();
+                    }
+                }
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadKey();
+            }
+
+        }
+
+        public static Assembly OnAssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            var assemblyName = new AssemblyName(args.Name);
+            string path = "";
+            //Check for referenced Siemens assemblies.
+            if (assemblyName.Name.EndsWith("Siemens.Engineering"))
+                path = plc;
+            if (assemblyName.Name.EndsWith("Siemens.Engineering.Hmi"))
+                path = hmi;
+            //Load Selected Siemens Assembly.
+            if (string.IsNullOrEmpty(path) == false
+                && File.Exists(path))
+            {
+                return Assembly.LoadFrom(path);
+            }
+            // return null for all other assemblies.
+            return null;
         }
     }
 }
---
+++ RegistryReader.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchivarTIA
+{
+    /// <summary>
+    /// Thanks to K_Ohjus of support.industry.siemens forum for this class!!
+    /// </summary>
+    class RegistryReader
+    {
+
+        private const string BASE_PATH = "SOFTWARE\\Siemens\\Automation\\Openness\\";
+
+        public static List<string> GetVersions()
+        {
+            RegistryKey key = GetRegistryKey(BASE_PATH);
+
+            if (key != null)
+            {
+                var names = key.GetSubKeyNames().OrderBy(x => x).ToList();
+                key.Dispose();
+
+                return names;
+            }
+
+            return new List<string>();
+        }
+        /// <summary>
+        /// Searches registry for all assemblies under selected TIA version.
+        /// </summary>
+        /// <param name="version">Selected assembly version</param>
+        /// <returns>List of assemblies</returns>
+        public static List<string> GetAssemblies(string version)
+        {
+            RegistryKey key = GetRegistryKey(BASE_PATH + version);
+
+            if (key != null)
+            {
+                try
+                {
+                    var subKey = key.OpenSubKey("PublicAPI");
+                    var result = subKey.GetSubKeyNames().OrderBy(x => x).ToList();
+
+                    subKey.Dispose();
+
+                    return result;
+                }
+                finally
+                {
+                    key.Dispose();
+                }
+            }
+
+            return new List<string>();
+        }
+        /// <summary>
+        /// Searches registry for assemblies outputs paths to Siemens.Engineering and Siemens.Engineering.Hmi DLLs.
+        /// </summary>
+        /// <param name="version">Selected assembly version</param>
+        /// <param name="assemblyPath">Path to Siemens.Engineering.DLL</param>
+        /// <param name="assemblyPathHmi">Path to Siemens.Engineering.Hmi.DLL</param>
+        public static void GetAssemblyPath(string version, string assembly, out string assemblyPath, out string assemblyPathHmi)
+        {
+            assemblyPath = "";
+            assemblyPathHmi = "";
+            RegistryKey key = GetRegistryKey(BASE_PATH + version + "\\PublicAPI\\" + assembly);
+
+            if (key != null)
+            {
+                try
+                {
+                    assemblyPath = key.GetValue("Siemens.Engineering").ToString();
+                    assemblyPathHmi = key.GetValue("Siemens.Engineering.Hmi").ToString();
+
+                }
+                finally
+                {
+                    key.Dispose();
+                }
+            }
+
+        }
+        /// <summary>
+        /// Gets any key from Registry.
+        /// </summary>
+        /// <param name="keyname">Selected assembly version</param>
+        /// /// <returns>key</returns>
+        private static RegistryKey GetRegistryKey(string keyname)
+        {
+            //HKEY_LOCAL_MACHINE
+            RegistryKey baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            RegistryKey key = baseKey.OpenSubKey(keyname);
+            //If key is not found in 64-bit registry
+            if (key == null)
+            {
+                baseKey.Dispose();
+                baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Default);
+                key = baseKey.OpenSubKey(keyname);
+            }
+            if (key == null)
+            {
+                baseKey.Dispose();
+                baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
+                key = baseKey.OpenSubKey(keyname);
+            }
+            baseKey.Dispose();
+
+            return key;
+
+        }
+    }
+}
